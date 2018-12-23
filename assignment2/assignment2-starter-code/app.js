1448273d51543068fb2862da538828eb63adde61
KalimdorTheKing <kayaarslanali@gmail.com>
2018-12-23 02:18:41 -0800

    Assignment 2 completed!

--- assignment2/assignment2-starter-code/app.js
+++ assignment2/assignment2-starter-code/app.js
@@ -1,4 +1,54 @@
 (function(){
   'use strict';
-  angular.module('Assignment2', [])
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService)
+  {
+    var buyList = this;
+
+    buyList.items = ShoppingListCheckOffService.getBuyItems();
+
+    buyList.buyItem = function(index){
+      ShoppingListCheckOffService.buyItem(index);
+    }
+  }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService)
+  {
+    var boughtList = this;
+
+    boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+  }
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+    var buyItems = [
+      { id: 0, name: "cookies", quantity: 10, bought: false},
+      { id: 1, name: "cakes", quantity: 2, bought: false },
+      { id: 2, name: "donuts", quantity: 20, bought: false },
+      { id: 3, name: "candies", quantity: 1, bought: false },
+      { id: 4, name: "ice creams", quantity: 4, bought: false }
+    ];
+
+    var boughtItems = [];
+
+    service.getBuyItems = function() {
+        return buyItems;
+    };
+
+    service.getBoughtItems = function() {
+        return boughtItems;
+    };
+
+    service.buyItem = function(index){
+      boughtItems.push(buyItems[index]);
+      buyItems.splice(index,1);
+    };
+  }
+
 })();
